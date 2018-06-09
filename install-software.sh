b9da90a8709ca2dc100961d1ea5d9c3293de75a8
Mark Molesworth <m.molesworth@gmail.com>
2018-06-09 11:16:33 -0400

    Create generic script to install and setup software.

---
+++ install-software.sh
@@ -0,0 +1,14 @@
+sudo apt-get install git emacs gnucash awscli wget curl sbcl 
+
+# Clone GitHub
+git clone https://github.com/mmolesworth/misc.git
+
+
+
+#mkdir software-downloads
+#cd software-downloads
+
+# Anaconda
+#wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+#sh Anaconda3-5.2.0-Linux-x86_64.sh
+
